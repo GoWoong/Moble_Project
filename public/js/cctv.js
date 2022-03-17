098f9d25baa548f7d838ffc0529cb57e284167fd
GoWoong <popgun10@naver.com>
2022-03-17 18:06:37 +0900

    cctv page

--- public/js/cctv.js
+++ public/js/cctv.js
@@ -1,5 +1,4 @@
 let image;
-
 async function imagedata() {
   await fetch("http://localhost:4000/managerPage/cctv/images")
     .then(function (response) {
@@ -10,19 +9,15 @@ async function imagedata() {
       image = JSON.parse(stringdata);
     });
 }
-
 async function cardUpdate() {
   await imagedata();
-  console.log(image);
-  console.log(typeof image);
   let card = document.getElementById("card");
   for (let i in image) {
-    console.log(image[i]);
     card.innerHTML =
       card.innerHTML +
       `
     <div class="card" style="width: 18rem">
-          <img src="/uploads/${image[i].imagename}" class="card-img-top" />
+          <img src="/${image[i].imagename}" class="card-img-top" alt "이미지"/>
           <div class="card-body">
             <h5 class="card-title">${image[i].imagename}</h5>
             <p class="card-text">
@@ -33,20 +28,6 @@ async function cardUpdate() {
   }
 }
 var getimages = document.getElementById("getimages");
-
 getimages.addEventListener("click", () => {
   cardUpdate();
 });
-
-{
-  /* <div class="card" style="width: 18rem">
-          <!-- <img src="..." class="card-img-top" alt="..." /> -->
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div> */
-}
